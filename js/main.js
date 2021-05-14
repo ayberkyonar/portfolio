bd959388c61918c1c630747aa4d85353540b4892
Ayberk Yonar <302155034@student.rocmondriaan.nl>
2021-05-14 21:35:36 +0200

    Portfolio

---
+++ js/main.js
@@ -0,0 +1,103 @@
+let headerButton = document.querySelector('body');
+let myButtons = document.querySelector('.changeTheme');
+
+function changeColor(){
+    headerButton.classList.toggle ('themeDark');
+}
+
+let display='0';
+let operator=null;
+let firstValue= 0;
+let secondValue= 0;
+let result= 0;
+
+let displayElement=document.querySelector('.display');
+let buttonsElement=document.querySelector('.buttons');
+
+buttonsElement.addEventListener('click', handleClick);
+showDisplay();
+
+function handleClick(event)
+{
+    const element=event.target;
+
+    switch(element.className)
+    {
+        case 'btn-number':
+            handleNumber(element);
+            break;
+        case 'btn-operator':
+            handleOperator(element);
+            break;  
+        case 'btn-dot':
+            handleDot(element);
+            break;
+        case 'btn-clear':
+            handleClear(element);
+            break;
+        case 'btn-equal':
+            handleEqual(element);
+            break;
+    }
+}
+
+function handleNumber(el)
+{
+    let number=el.innerHTML;
+  
+    if(display==='0'){
+        display=number;
+    }
+    else{
+        display=display+number;
+    }
+
+    showDisplay();
+} 
+
+function handleOperator(el) {
+    operator=el.innerHTML;
+    firstValue = display;
+    display = "0";
+    showDisplay();
+}
+
+function handleDot(el){
+    let dot = el.innerHTML;
+    if(!display.includes(".")){
+        display=display+dot;
+    }
+    showDisplay();
+}
+
+function handleClear (el) {
+    display='0';
+    operator=null;
+    firstValue= 0;
+    secondValue= 0;
+    result= 0;
+    displayElement.innerHTML = 0;
+    showDisplay;
+}
+
+function handleEqual () {
+    secondValue=display;
+    display = "0";
+    let valueOne = parseFloat(firstValue);
+    let valueTwo = parseFloat(secondValue);
+    if (operator === "+") {
+        result = valueOne+valueTwo;
+    }else if(operator === "-") {
+        result = valueOne-valueTwo;
+    }else if(operator === "*") {
+        result = valueOne*valueTwo;
+    }else if(operator === "/") {
+        result = valueOne/valueTwo;
+    }
+    display=result;
+    showDisplay();
+}
+
+function showDisplay(){
+    displayElement.innerHTML=display;
+}
